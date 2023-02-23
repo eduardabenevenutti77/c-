96e43a66b2eabb550b38b1997c8d169b4244c3c7
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-02-23 19:13:50 -0300

    Add files via upload

---
+++ c++/lista_maria_exer2.cpp
@@ -0,0 +1,14 @@
+//leia as horas trabalhadas e o valor delas e informe o falor que deverá receber no final do mes
+#include<iostream>
+using namespace std;
+int valor,tempo,recebe,mes;
+main(){
+	system("chcp 65001");
+	cout<<"qual é o valor da hora trabalhada?";
+	cin>>valor;
+	cout<<"qual é o tempo trabalhado?";
+	cin>>tempo;
+	recebe = valor * tempo;
+	mes =  recebe * 31;
+	cout<<"O valor de horas trabalhadas é "<<valor<<" e o tempo de horas trabalhada é "<<tempo<<" ao mes, portanto, voce deverá receber "<<mes<<".";
+}
