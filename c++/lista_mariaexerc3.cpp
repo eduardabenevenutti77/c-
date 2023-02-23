d48b985bbf925ec21d0b6979fd25f31d3b16ba3d
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-02-23 12:03:58 -0300

    Add files via upload

--- c++/lista_mariaexerc3.cpp
+++ c++/lista_mariaexerc3.cpp
@@ -1,9 +1,11 @@
 //imprima a tabuada
 #include<iostream>
+#include<locale>
 using namespace std;
 int tabuada,tab,tab1,tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10;
 main (){
-    cout<<"informe a tabuada que deverá ser copiada:";
+    system ("chcp 65001");
+	cout<<"informe a tabuada que deverá ser copiada:";
     cin>>tabuada;
     tab = tabuada * 0;
     tab1 = tabuada * 1;
@@ -17,8 +19,16 @@ main (){
 	tab9 = tabuada * 9;
 	tab10 = tabuada * 10; 
 	cout<<"Tabuada:"<<tabuada"X0:"<<tab<<;
-	cout<<tabuada"X1:"<<tab1<<;
-	
+	cout<<tabuada<<"X1:"<<tab1<<;
+	cout<<tabuada<<"X2:"<<tab2<<;
+	cout<<tabuada<<"X3:"<<tab3<<;
+	cout<<tabuada<<"X4:"<<tab4<<;
+	cout<<tabuada<<"X5:"<<tab5<<;
+	cout<<tabuada<<"X6:"<<tab6<<;
+	cout<<tabuada<<"X7:"<<tab7<<;
+	cout<<tabuada<<"X8:"<<tab8<<;
+	cout<<tabuada<<"X9:"<<tab9<<;
+	cout<<tabuada<<"X10:"<<tab10<<;	
 }
 
 
