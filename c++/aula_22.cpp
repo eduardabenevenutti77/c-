8c90f3d6e27561081f2f658e507481e056f7704d
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-02-22 21:13:59 -0300

    Add files via upload

--- c++/aula_22.cpp
+++ c++/aula_22.cpp
@@ -5,14 +5,14 @@ int idade,resp1,ano,ano1; //é uma variavel inteira
 main (){
 	system("chcp 65001");
 	
-	cout<<"\nquantos anos vc terá em 2072!\n";
+	cout<<"\nquantos anos vc terá no futuro!\n";
 	cout<<"qual ano vc nasceu?";
 	cin>>ano;
-	cout<<"\n qual ano vc deseja descobrir a sua idade?";
+	cout<<"em qual ano vc deseja descobrir a sua idade?";
 	cin>>ano1; //variavel
 	resp1 = ano1 - ano;
 	//resp = idade+2072-2023; //efetua a operação e transforma o resultado em resp
 	
-	cout<<"vc nasceu em "<<ano<<" vc terá" <<ano1<<", vc terá "<<resp1<<".";
+	cout<<"\nvocê nasceu em "<<ano<<" e em " <<ano1<<", você terá "<<resp1<<" de idade.";
 	cout<<"\n\n\n\n";
 }  
