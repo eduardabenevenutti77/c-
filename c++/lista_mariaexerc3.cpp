b67dbb297101f1690d1cac1ac658b5132b54dad4
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-02-23 08:07:55 -0300

    Add files via upload

---
+++ c++/lista_mariaexerc3.cpp
@@ -0,0 +1,24 @@
+//imprima a tabuada
+#include<iostream>
+using namespace std;
+int tabuada,tab,tab1,tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10;
+main (){
+    cout<<"informe a tabuada que deverá ser copiada:";
+    cin>>tabuada;
+    tab = tabuada * 0;
+    tab1 = tabuada * 1;
+    tab2 = tabuada * 2;
+    tab3 = tabuada * 3;
+	tab4 = tabuada * 4;
+	tab5 = tabuada * 5;
+	tab6 = tabuada * 6;
+	tab7 = tabuada * 7;
+	tab8 = tabuada * 8;
+	tab9 = tabuada * 9;
+	tab10 = tabuada * 10; 
+	cout<<"Tabuada:"<<tabuada"X0:"<<tab<<;
+	cout<<tabuada"X1:"<<tab1<<;
+	
+}
+
+
---
+++ c++/lista_mariaexerc4.cpp
@@ -0,0 +1,13 @@
+//imprama o valor bruto e com desconto do inss
+#include<iostream>
+using namespace std;
+int salario,desc,desc1,valor_desc;
+main (){
+ 	system("chcp 65001");
+ 	cout<<"qual é o seu salário?";
+ 	cin>>salario;
+ 	desc = salario * 13;
+ 	desc1 = desc / 100;
+ 	valor_desc = salario - desc1;
+ 	cout<<"o seu salário é de "<<salario<<", portanto o seu desconto é de "<<desc1<<" reais. Sendo assim, o valor final é de"<<valor_desc<<"";
+}
