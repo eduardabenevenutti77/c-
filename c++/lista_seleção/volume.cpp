1a8a77292fd1afbd5d6fa404aaf3fd9336fb9a65
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-03-04 08:45:59 -0300

    lista-quarta_feira

--- c++/lista_seleção/volume.cpp
+++ c++/lista_seleção/volume.cpp
@@ -3,16 +3,17 @@ using namespace std;
 int r,altura,volume;
 main (){
 	system("chcp 65001");
+	system("cls");
 	cout<<"\n\n";
 	cout<<"informe o valor do raio:";
 	cin>>r;
 	cout<<"informe o valor da altura:";
 	cin>>altura;
-	volume=3.14*rÂ²*altura;
+	volume=3.14*(r**2)*altura;
 	if(volume)
 	{
-		cout<<"a altura da lata de Ã³leo Ã© "<<altura<<", e ela tem o raio de "<<r<<" cm";
-		cout<<"portanto, o volume Ã© "<<volume<<"";
+		cout<<"a altura da lata de óleo é "<<altura<<", e ela tem o raio de "<<r<<" cm";
+		cout<<"portanto, o volume é "<<volume<<"";
 	}
 	cout<<"\n\n";
 }
