ff2aaa37e0cacb0dd9ef9fdc6043a8addf88da0b
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-03-17 09:15:34 -0300

    c++

--- c++/repetição/repetição/multiplos5.cpp
+++ c++/repetição/repetição/multiplos5.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
-int mult,cont_mult;
+int mult,soma;
 main(){
 	system("chcp 65001");
 	cout<<"\n\n";
 	
-	cout<<"calcular a soma dos números ímpares que são múltiplos de cinco e que se encontram entre 1 a 1000\n";
+	cout<<"calcular a soma dos numeros impares que sao multiplos de cinco e que se encontram entre 1 a 1000\n";
 	
-	for(int i=1;i<1000;i++){
-		if(mult%5==0){
-			cont_mult = cont_mult + mult;
+	for(int i=1;i<=1000;i++){
+		if(i%2==1 && i%5==0){ //faz a soma da quantidade de numeros impares que existem entre 1 a 1000
+			soma += 1;
 		}
 	}
-	cout<<"\nmúltiplos de 5:"<<cont_mult<<"";
+	cout<<"\na soma dos numeros impares|multiplos de 5 que estao entre 1 a 1000 : "<<soma<<"";
 }
