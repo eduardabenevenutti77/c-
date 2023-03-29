b640a8dfcf01d130df78a2397f6d7373745053ab
Maria Eduarda Benevenutti <115738167+eduardabenevenutti77@users.noreply.github.com>
2023-03-29 20:53:28 -0300

    Add files via upload

--- c++/PROVA_1SEMESTRE/convercao_numerica.cpp
+++ c++/PROVA_1SEMESTRE/convercao_numerica.cpp
@@ -1,11 +1,11 @@
 /* 
 Faculdade SENAC - Joinville - SC
-Profª: Cláudia Werlich
-Atividade Avaliativa de Introdução a Programação de Computadores
+ProfÂª: ClÃ¡udia Werlich
+Atividade Avaliativa de IntroduÃ§Ã£o a ProgramaÃ§Ã£o de Computadores
 
 Insira Seu nome completo aqui:Maria Eduarda Benevenutti
 
-Data: <<versÃ£o 01 ou versão_final>> :___________________________ 
+Data: <<versÃƒÂ£o 01 ou versÃ£o_final>> :___________________________ 
 (Altere a data conforme a entrega)
 
 */
@@ -15,24 +15,23 @@ Data: <<versÃ£o 01 ou versão_final>> :___________________________
 #include <iomanip>
 #include <conio.h> // Para usar o getch();
 
-float t1,t;
+float t1,c,c2,c3,c4,c5,c6;
 int op;
 using namespace std;
 main ()
 {
    system("chcp 65001");// para usar acentos
-   setlocale(LC_ALL, "portuguese");
    cout << fixed << setprecision(2);// deixar o float com 2 casas decimais
    system ("cls"); // limpar a tela
    system ("color 2"); // usar a cor verde
-   cout<<"ATIVIDADE AVALIATIVA DE INTRODUÇÃO A PROGRAMAÇÃO \n";
+   cout<<"ATIVIDADE AVALIATIVA DE INTRODUÃ‡ÃƒO A PROGRAMAÃ‡ÃƒO \n";
    cout<<"INDICADORES: \n";
-   cout<<"1. Realiza operações matemáticas de acordo com os fundamentos e o contexto apresentado.\n";
-   cout<<"2. Utiliza adequadamente os tipos de variáveis em algoritmos de acordo com o contexto.\n";
-   cout<<"3. Utiliza operações aritméticas em programas.\n";
+   cout<<"1. Realiza operaÃ§Ãµes matemÃ¡ticas de acordo com os fundamentos e o contexto apresentado.\n";
+   cout<<"2. Utiliza adequadamente os tipos de variÃ¡veis em algoritmos de acordo com o contexto.\n";
+   cout<<"3. Utiliza operaÃ§Ãµes aritmÃ©ticas em programas.\n";
    cout<<"4. Utiliza os comandos de leitura e escrita em algoritmos de acordo com o contexto.\n";
    cout<<"5. Utiliza as estruturas condicionais em algoritmos de acordo com o contexto.\n";
-   cout<<"6. Utiliza as estruturas de repetição em algoritmos de acordo com o contexto. \n";
+   cout<<"6. Utiliza as estruturas de repetiÃ§Ã£o em algoritmos de acordo com o contexto. \n";
    
    
    do{
@@ -40,62 +39,77 @@ main ()
    cout<<"\n\t\tUnidades de medidadas";
    cout<<"\n [1] QuestÃ£o 01 - << Converta o valor para massa>>";
    cout<<"\n [2] QuestÃ£o 02 - << Converta o valor para comprimento>>";
-   //cout<<"\n [3] QuestÃ£o 03 - << >>";
-   //cout<<"\n [4] QuestÃ£o 04 - << >>";
-   //cout<<"\n [5] QuestÃ£o 05 - << >>";
-   cout<<"\n [3] FIM <<Bônus >>.";
-   cout<<"\n\n\n\n ESCOLHA UMA OPÇÃO --> ";
+   cout<<"\n [3] FIM <<BÃ´nus >>.";
+   cout<<"\n\n\n\n ESCOLHA UMA OPÃ‡ÃƒO --> ";
    cin>>op;
   switch (op)
   {
     case  1:{
-             system ("color 2");
-             cout<<"\n Converta o valor para massa";
-             cout<<" \n Informe o valor que deseja converter de kg para hg? ";
+             system ("color D");
+             cout<<"\n Converta o valor para massa\n";
+             cout<<"\n Informe o valor que deseja converter de kg para hg? ";
              cin>>t1;
-             for(int i=0;i<=t1;i++){
-			 
-             t = t1*10;
-         }
+             c = t1*10;
+             cout<<"\n\t â€¢ o valor Ã©: "<<c<<" hg;\n";
+             
+             cout<<"\n o valor " <<c<<" convertido em dag:";
+             c2 = c*10;
+             cout<<"\n\n\t â€¢ o valor Ã©: "<<c2<<" dag;\n";
+             
+             cout<<"\n o valor: "<<c2<<" convertido em g:\n";
+             c3 = c2*10;
+             cout<<"\n\t â€¢ o valor Ã©: "<<c3<<" g;\n";
+             
+             cout<<"\n o valor:"<<c3<<" convertido para dg:\n ";
+             c4 = c3*10;
+             cout<<"\n\t â€¢ o valor Ã©: "<<c4<<" dg;\n";
+             
+             cout<<"\n o valor: "<<c4<<" para converter em cg:\n ";
+             c5 = c4*10;
+             cout<<"\n\t â€¢ o valor Ã©: "<<c5<<" cg;\n";
+             
+             cout<<"\n o valor: "<<c5<<" convertido em mg:\n ";
+             c6 = c5*10;
+             cout<<"\n\t â€¢ o valor Ã©: "<<c6<<" mg;";
+             
              getch(); // espera digitar algo
              break;
            }
     case 2:{
              system ("color 3");
              cout<<"\n Converta o valor para comprimento";
-             cout<<" \n APERTE UMA TECLA";
              getch(); //ou system("pause");
              break;
            }
     //case 3:{
              //system ("color 3");
-             //cout<<"\n OpÃ§Ã£o 03";
+             //cout<<"\n OpÃƒÂ§ÃƒÂ£o 03";
              //cout<<" \n APERTE UMA TECLA";
              //getch(); //ou system("pause");
              //break;
            //}
     //case 4:{
              //system ("color 3");
-             //cout<<"\n OpÃ§Ã£o 04";
+             //cout<<"\n OpÃƒÂ§ÃƒÂ£o 04";
              //cout<<" \n APERTE UMA TECLA";
              //getch(); //ou system("pause");
              //break;
            //}
     //case 5:{
              //system ("color 3");
-             //cout<<"\n OpÃ§Ã£o 02";
+             //cout<<"\n OpÃƒÂ§ÃƒÂ£o 02";
              //cout<<" \n APERTE UMA TECLA";
              //getch(); //ou system("pause");
              //break;
            //}
     case 3:{
              system ("color E");
-             cout<<"\nSah ein Knab’ ein Röslein stehn,\nRöslein auf der Heiden,\nWar so jung und morgenschön,\nLief er schnell es nah zu sehn,\nSah’s mit vielen Freuden.\nRöslein, Röslein, Röslein rot,\nRöslein auf der Heiden.\n\nKnabe sprach: “Ich breche dich,\nRöslein auf der Heiden.”\nRöslein sprach: “Ich steche dich,\nDass du ewig denkst an mich,\nUnd ich will’s nicht leiden.\nRöslein, Röslein, Röslein rot,\nRöslein auf der Heiden\n\nUnd der wilde Knabe brach\n‘s Röslein auf der Heiden.\nRöslein wehrte sich und stach,\nHalf ihm doch kein Weh und Ach,\nMusst es eben leiden.\nRöslein, Röslein, Röslein rot,\nRöslein auf der Heiden.";
+             cout<<"\n\tHEIDENRÃ–SLEIN\n\nSah ein Knabâ€™ ein RÃ¶slein stehn,\nRÃ¶slein auf der Heiden,\nWar so jung und morgenschÃ¶n,\nLief er schnell es nah zu sehn,\nSahâ€™s mit vielen Freuden.\nRÃ¶slein, RÃ¶slein, RÃ¶slein rot,\nRÃ¶slein auf der Heiden.\n\nKnabe sprach: â€œIch breche dich,\nRÃ¶slein auf der Heiden.â€\nRÃ¶slein sprach: â€œIch steche dich,\nDass du ewig denkst an mich,\nUnd ich willâ€™s nicht leidenâ€.\nRÃ¶slein, RÃ¶slein, RÃ¶slein rot,\nRÃ¶slein auf der Heiden\n\nUnd der wilde Knabe brach\nâ€˜s RÃ¶slein auf der Heiden.\nRÃ¶slein wehrte sich und stach,\nHalf ihm doch kein Weh und Ach,\nMusst es eben leiden.\nRÃ¶slein, RÃ¶slein, RÃ¶slein rot,\nRÃ¶slein auf der Heiden.\n\nJohan Wolfang von Goethe";
              getch();
              break;
             }
    default: {
-               cout<<"\n Informou a opção errada";
+               cout<<"\n Informou a opÃ§Ã£o errada";
                getch();
                break;
             }
